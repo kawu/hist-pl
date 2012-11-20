72d4e2c5e8acb5ffcedf72d79b1a875a14304e0c
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-11-20 18:43:36 +0100

    Add info about form into binary keys (done)

--- lexicon/NLP/Polh/Binary.hs
+++ lexicon/NLP/Polh/Binary.hs
@@ -7,28 +7,26 @@
 -- representation of the historical dictionary of Polish.
 
 module NLP.Polh.Binary
-( Key
-, lexKey
+( Key (..)
 
 , savePolh
 , loadPolh
 
--- , PolhT
--- , runPolhT
--- , PolhM
--- , runPolh
--- , index
--- , withKey
--- , lookup
+, PolhT
+, runPolhT
+, PolhM
+, runPolh
+, index
+, withKey
+, lookup
 ) where
 
 import Prelude hiding (lookup)
 import Control.Exception (try, SomeException)
-import Control.Applicative ((<$>), (<*>))
+import Control.Applicative (Applicative, (<$>), (<*>))
 import Control.Monad (when, guard)
 import Control.Monad.Trans.Class (MonadTrans)
 import Control.Monad.IO.Class (liftIO, MonadIO)
-import Control.Applicative (Applicative, (<$>))
 import Control.Monad.Reader (ReaderT (..), ask, lift)
 import Control.Monad.Trans.Maybe (MaybeT (..))
 import System.IO.Unsafe (unsafeInterleaveIO)
@@ -37,9 +35,7 @@ import System.Directory ( getDirectoryContents, createDirectoryIfMissing
                         , createDirectory, doesDirectoryExist )
 import Data.Maybe (catMaybes)
 import Data.List (mapAccumL)
-import Data.Monoid (mappend, mconcat)
 import Data.Binary (Binary, get, put, encodeFile, decodeFile)
-import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.DAWG as D
@@ -55,21 +51,13 @@ entryDir = "entries"
 formMapFile :: String
 formMapFile = "forms.bin"
 
--- | A dictionary key.
-data Key = Key {
-    -- | First form (presumably lemma) of the lexical entry.
-    TUTAJ SKONCZYLEM
-      keyBase   :: T.Text
-    -- | Unique identifier of the entry.
-    , keyUid    :: Int }
-    deriving (Show, Eq, Ord)
-
 -- | Entry in the binary dictionary consists of the lexical
 -- entry and corresponding unique identifier.
 data BinEntry = BinEntry {
     -- | Lexical entry.
       entry :: LexEntry
-    -- | Unique identifier among lexical entries with the same base form.
+    -- | Unique identifier among lexical entries with the same first form
+    -- (see 'Key' data type).
     , uid   :: Int }
     deriving (Show, Eq, Ord)
 
@@ -77,14 +65,34 @@ instance Binary BinEntry where
     put BinEntry{..} = put entry >> put uid
     get = BinEntry <$> get <*> get
 
+-- | A dictionary key which uniquely identifies the lexical entry.
+data Key = Key {
+    -- | First form (presumably lemma) of the lexical entry.
+      keyForm   :: T.Text
+    -- | Unique identifier among lexical entries with the same 'keyForm'.
+    , keyUid    :: Int }
+    deriving (Show, Eq, Ord)
+
+-- | Form representing the lexical entry.
+proxyForm :: LexEntry -> T.Text
+proxyForm entry = case Util.allForms entry of
+    (x:_)   -> x
+    []      -> error "proxyForm: entry with no forms"
+
 -- | Key assigned to the binary entry.
 binKey :: BinEntry -> Key
-binKey BinEntry{..} = Key (lemma entry) uid
+binKey BinEntry{..} = Key (proxyForm entry) uid
 
 -- | Convert the key to the path where binary representation of the entry
 -- is stored.
-keyPath :: Key -> String
-keyPath Key{..} = T.concat [keyBase, "-", T.pack (show keyUid)]
+showKey :: Key -> String
+showKey Key{..} = (T.unpack . T.concat) [T.pack (show keyUid), "-", keyForm]
+
+-- | Parse the key.
+parseKey :: String -> Key
+parseKey x =
+    let (uid'S, (_:form'S)) = break (=='-') x
+    in  Key (T.pack form'S) (read uid'S)
 
 -- | Load the directory contents.
 loadContents :: FilePath -> IO [FilePath]
@@ -98,15 +106,15 @@ emptyDirectory path = null <$> loadContents path
 
 -- | Save the binary entry on the disk.
 saveLexEntry :: FilePath -> BinEntry -> IO ()
-saveLexEntry path x = 
-    let binPath = keyPath . binKey
+saveLexEntry path x =
+    let binPath = showKey . binKey
     in  encodeFile (path </> binPath x) x
 
 withUid :: D.DAWG Int -> LexEntry -> (D.DAWG Int, BinEntry)
 withUid m x =
-    let key = T.unpack (lemma x)
-        num = maybe 0 id (D.lookup key m) + 1
-    in  (D.insert key num m, BinEntry x num)
+    let path = T.unpack (proxyForm x)
+        num  = maybe 0 id (D.lookup path m) + 1
+    in  (D.insert path num m, BinEntry x num)
 
 mapIO'Lazy :: (a -> IO b) -> [a] -> IO [b]
 mapIO'Lazy f (x:xs) = (:) <$> f x <*> unsafeInterleaveIO (mapIO'Lazy f xs)
@@ -125,16 +133,16 @@ savePolh path xs = do
         <$> mapIO'Lazy (saveLex lexPath) (withUids xs)
     encodeFile (path </> formMapFile) formMap'
   where
-    withUids = mapAccumL withUid
+    withUids = snd . mapAccumL withUid D.empty
     saveLex lexPath x = do
         saveLexEntry lexPath x
         return $ rules x
     rules binEntry =
-        [ (x, S.singleton (between x key))
-        | x <- Util.allForms lex ]
+        [ ( T.unpack x
+          , S.singleton (between x key) )
+        | x <- Util.allForms (entry binEntry) ]
       where
         key = binKey binEntry
-        lex = entry binEntry
 
 maybeErr :: MonadIO m => IO a -> m (Maybe a)
 maybeErr io = do
@@ -155,7 +163,7 @@ maybeErrT io = do
 -- | Load lexical entry from disk by its key.
 loadLexEntry :: FilePath -> Key -> IO (Maybe LexEntry)
 loadLexEntry path key = do
-    maybeErr $ decodeFile (path </> T.unpack key)
+    maybeErr $ decodeFile (path </> showKey key)
 
 -- | A rule for translating a form into a binary dictionary key.
 data Rule = Rule {
@@ -167,6 +175,10 @@ data Rule = Rule {
     , ruleUid   :: !Int }
     deriving (Show, Eq, Ord)
 
+instance Binary Rule where
+    put Rule{..} = put cut >> put suffix >> put ruleUid
+    get = Rule <$> get <*> get <*> get
+
 -- | Apply the rule.
 apply :: Rule -> T.Text -> Key
 apply r x =
@@ -176,8 +188,8 @@ apply r x =
 -- | Make a rule which translates between the string and the key.
 between :: T.Text -> Key -> Rule
 between source dest =
-    let k = lcp source (keyBase dest)
-    in  Rule (T.length source - k) (T.drop k (keyBase dest)) (keyUid dest)
+    let k = lcp source (keyForm dest)
+    in  Rule (T.length source - k) (T.drop k (keyForm dest)) (keyUid dest)
   where
     lcp a b = case T.commonPrefixes a b of
         Just (c, _, _)  -> T.length c
@@ -186,75 +198,75 @@ between source dest =
 -- | Binary dictionary data kept in program memory.
 data MemData = MemData
     { polhPath  :: FilePath
-    , formMap   :: D.DAWG (S.Set Key) }
+    , formMap   :: D.DAWG (S.Set Rule) }
 
--- -- | A Polh monad transformer.
--- newtype PolhT m a = PolhT (ReaderT MemData m a)
---     deriving (Functor, Applicative, Monad, MonadTrans, MonadIO)
+-- | A Polh monad transformer.
+newtype PolhT m a = PolhT (ReaderT MemData m a)
+    deriving (Functor, Applicative, Monad, MonadTrans, MonadIO)
+
+-- | A Polh monad is a Polh monad transformer over the hidden IO monad.
+type PolhM a = PolhT IO a
+
+-- | Path to directory with entries.
+entryPath :: MemData -> FilePath
+entryPath = (</> entryDir) . polhPath
+
+-- | List of dictionary keys.
+index :: (Applicative m, MonadIO m) => PolhT m [Key]
+index = PolhT $ do
+    path <- entryPath <$> ask
+    map parseKey <$> liftIO (loadContents path)
+
+-- | Extract lexical entry with the given key.
+withKey :: (Applicative m, MonadIO m) => Key -> PolhT m (Maybe LexEntry)
+withKey key = PolhT $ do
+    path <- entryPath <$> ask
+    liftIO . unsafeInterleaveIO $ loadLexEntry path key
+
+-- | Lookup the form in the dictionary.
+lookup :: (Applicative m, MonadIO m) => T.Text -> PolhT m [LexEntry]
+lookup x = do
+    fm <- PolhT $ formMap <$> ask
+    keys <- return $ case D.lookup (T.unpack x) fm of
+        Nothing -> []
+        Just xs -> map (flip apply x) (S.toList xs)
+    catMaybes <$> mapM withKey keys
+
+-- | Execute the Polh monad against the binary Polh representation
+-- located in the given directory.  Return Nothing if the directory
+-- doesnt' exist or if it doesn't look like a Polh dictionary.
+runPolh :: FilePath -> PolhM a -> IO (Maybe a)
+runPolh path polh = runPolhT path polh
+
+-- | Execute the Polh monad transformer against the binary Polh representation
+-- located in the given directory.  Return Nothing if the directory doesnt'
+-- exist or if it doesn't look like a Polh dictionary.
+runPolhT :: MonadIO m => FilePath -> PolhT m a -> m (Maybe a)
+runPolhT path (PolhT r) = runMaybeT $ do
+    formMap' <- maybeErrT $ decodeFile (path </> formMapFile)
+    doesExist <- liftIO $ doesDirectoryExist (path </> entryDir)
+    guard doesExist 
+    lift $ runReaderT r (MemData path formMap')
+
+-- | Load dictionary from a disk in a lazy manner.  Return 'Nothing'
+-- if the path doesn't correspond to a binary representation of the
+-- dictionary. 
+loadPolh :: FilePath -> IO (Maybe Polh)
+loadPolh path = runPolhT path $ do
+    keys <- index
+    catMaybes <$> mapM withKey keys
+
+-- We don't provide update functionality since we want only the pure
+-- iterface to be visible.  It greatly simplifies the implementation.
+--
+-- updateLexEntry :: FilePath -> Key -> (LexEntry -> LexEntry) -> IO LexEntry
+-- updateLexEntry path lexKey f = do
+--     lexEntry <- loadLexEntry path lexKey
+--     let lexEntry' = f lexEntry
+--     saveLexEntry path lexEntry'
+--     return lexEntry'
 -- 
--- -- | A Polh monad is a Polh monad transformer over the hidden IO monad.
--- type PolhM a = PolhT IO a
--- 
--- -- | Path to directory with entries.
--- entryPath :: MemData -> FilePath
--- entryPath = (</> entryDir) . polhPath
--- 
--- -- | List of dictionary keys.
--- index :: (Applicative m, MonadIO m) => PolhT m [Key]
--- index = PolhT $ do
---     path <- entryPath <$> ask
---     map T.pack <$> liftIO (loadContents path)
--- 
--- -- | Extract lexical entry with the given ID.
--- withKey :: (Applicative m, MonadIO m) => Key -> PolhT m (Maybe LexEntry)
--- withKey key = PolhT $ do
---     path <- entryPath <$> ask
---     liftIO . unsafeInterleaveIO $ loadLexEntry path key
--- 
--- -- | Lookup the form in the dictionary.
--- lookup :: (Applicative m, MonadIO m) => T.Text -> PolhT m [LexEntry]
--- lookup x = do
---     fm <- PolhT $ formMap <$> ask
---     keys <- return $ case M.lookup x fm of
---         Nothing -> []
---         Just xs -> S.toList xs
---     catMaybes <$> mapM withKey keys
--- 
--- -- | Execute the Polh monad against the binary Polh representation
--- -- located in the given directory.  Return Nothing if the directory
--- -- doesnt' exist or if it doesn't look like a Polh dictionary.
--- runPolh :: FilePath -> PolhM a -> IO (Maybe a)
--- runPolh path polh = runPolhT path polh
--- 
--- -- | Execute the Polh monad transformer against the binary Polh representation
--- -- located in the given directory.  Return Nothing if the directory doesnt'
--- -- exist or if it doesn't look like a Polh dictionary.
--- runPolhT :: MonadIO m => FilePath -> PolhT m a -> m (Maybe a)
--- runPolhT path (PolhT r) = runMaybeT $ do
---     formMap' <- maybeErrT $ decodeFile (path </> formMapFile)
---     doesExist <- liftIO $ doesDirectoryExist (path </> entryDir)
---     guard doesExist 
---     lift $ runReaderT r (MemData path formMap')
--- 
--- -- | Load dictionary from a disk in a lazy manner.  Return 'Nothing'
--- -- if the path doesn't correspond to a binary representation of the
--- -- dictionary. 
--- loadPolh :: FilePath -> IO (Maybe Polh)
--- loadPolh path = runPolhT path $ do
---     keys <- index
---     catMaybes <$> mapM withKey keys
--- 
--- -- We don't provide update functionality since we want only the pure
--- -- iterface to be visible.  It greatly simplifies the implementation.
--- --
--- -- updateLexEntry :: FilePath -> Key -> (LexEntry -> LexEntry) -> IO LexEntry
--- -- updateLexEntry path lexKey f = do
--- --     lexEntry <- loadLexEntry path lexKey
--- --     let lexEntry' = f lexEntry
--- --     saveLexEntry path lexEntry'
--- --     return lexEntry'
--- -- 
--- -- updateLexEntry_ :: FilePath -> Key -> (LexEntry -> LexEntry) -> IO ()
--- -- updateLexEntry_ path lexKey f = do
--- --     lexEntry <- loadLexEntry path lexKey
--- --     saveLexEntry path (f lexEntry)
+-- updateLexEntry_ :: FilePath -> Key -> (LexEntry -> LexEntry) -> IO ()
+-- updateLexEntry_ path lexKey f = do
+--     lexEntry <- loadLexEntry path lexKey
+--     saveLexEntry path (f lexEntry)
