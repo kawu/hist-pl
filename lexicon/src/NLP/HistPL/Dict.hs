59774afa0281d3192c3f0ef3d066fe73aded7ddb
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-05-10 15:06:27 +0200

    Change Val from type synonym to a data type

--- lexicon/src/NLP/HistPL/Dict.hs
+++ lexicon/src/NLP/HistPL/Dict.hs
@@ -17,7 +17,11 @@ module NLP.HistPL.Dict
 -- ** Entry
 , Lex (..)
 , Key (..)
-, Val
+, Val (..)
+-- ** Entry set
+, LexSet
+, mkLexSet
+, unLexSet
 -- , encode
 , decode
 -- ** Query
@@ -32,7 +36,7 @@ module NLP.HistPL.Dict
 
 import Prelude hiding (lookup)
 import Control.Applicative ((<$>), (<*>))
-import Control.Arrow (first, second)
+import Control.Arrow (first)
 import Data.Binary (Binary, get, put)
 import Data.Text.Binary ()
 import qualified Data.Map as M
@@ -90,15 +94,25 @@ data Key i = Key {
 
 
 -- | A value of the entry.
-type Val a w b = (a, M.Map w b)
+data Val a w b = Val {
+    -- | Additional information assigned to the entry.
+      info  :: a
+    -- | A map of forms with additional info of type @b@ assigned.
+    , forms :: M.Map w b }
+    deriving (Show, Eq, Ord)
+
+
+instance (Ord w, Binary a, Binary w, Binary b) => Binary (Val a w b) where
+    put Val{..} = put info >> put forms
+    get = Val <$> get <*> get
 
 
 -- | A dictionary entry consists of a `Key` and a `Val`ue.
 data Lex i a b = Lex {
     -- | Entry key.
-      key :: Key i
+      lexKey :: Key i
     -- | Entry value.
-    , val :: Val a T.Text b }
+    , lexVal :: Val a T.Text b }
     deriving (Show, Eq, Ord)
 
 
@@ -106,14 +120,26 @@ data Lex i a b = Lex {
 type LexSet i a b = M.Map (Key i) (Val a T.Text b)
 
 
+-- | Make lexical set from a list of entries.
+mkLexSet :: Ord i => [Lex i a b] -> LexSet i a b
+mkLexSet = M.fromList . map ((,) <$> lexKey <*> lexVal)
+
+
+-- | List lexical entries.
+unLexSet :: LexSet i a b -> [Lex i a b]
+unLexSet = map (uncurry Lex) . M.toList
+
+
 -- | Actual values stored in automaton states contain
 -- all information but `orth`.
-type Node i a b = M.Map i (a, M.Map Rule b)
+type Node i a b = M.Map i (Val a Rule b)
 
 
 -- | Map function over entry word forms.
 mapW :: Ord w' => (w -> w') -> Val a w b -> Val a w' b
-mapW f = second $ M.fromList . map (first f) . M.toList
+mapW f v =
+    let g = M.fromList . map (first f) . M.toList
+    in  v { forms = g (forms v) }
 
 
 -- | Encode dictionary value given `orth`.
@@ -133,8 +159,8 @@ decode x n = M.fromList
 
 -- | Transform entry into a list.
 toListE :: Lex i a b -> [(T.Text, i, a, T.Text, b)]
-toListE (Lex Key{..} (x, forms)) =
-    [ (orth, uid, x, form, y)
+toListE (Lex Key{..} Val{..}) =
+    [ (orth, uid, info, form, y)
     | (form, y) <- M.assocs forms ]
 
 
@@ -156,8 +182,7 @@ lookup x dict = decode x $ case D.lookup (T.unpack x) dict of
 -- | List dictionary lexical entries.
 entries :: Ord i => Dict i a b -> [Lex i a b]
 entries = concatMap f . D.assocs where
-    f (key, val) = listIt $ decode (T.pack key) val
-    listIt = map (uncurry Lex) . M.toList
+    f (key, val) = unLexSet $ decode (T.pack key) val
 
 
 -- | Make dictionary from a list of (key, ID, entry info, form,
@@ -165,11 +190,11 @@ entries = concatMap f . D.assocs where
 fromList :: (Ord i, Ord a, Ord b) => [(T.Text, i, a, T.Text, b)] -> Dict i a b
 fromList xs = D.fromListWith union $
     [ ( T.unpack x
-      , M.singleton i (a, M.singleton (between x y) b) )
+      , M.singleton i (Val a (M.singleton (between x y) b)) )
     | (x, i, a, y, b) <- xs ]
   where
     union = M.unionWith $ both const M.union
-    both f g (x, y) (x', y') = (f x x', g y y')
+    both f g (Val x y) (Val x' y') = Val (f x x') (g y y')
 
 
 -- | Transform dictionary back into the list of (key, ID, key\/ID info, elem,
