f45fe8823610ab99535f1e69f9cbcc429252fecf
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-05-24 12:20:47 +0200

    Lookup by form

--- website/src/Main.hs
+++ website/src/Main.hs
@@ -13,6 +13,7 @@ import           Snap.Snaplet.Heist
 import           Snap.Util.FileServe (serveDirectory)
 import           Heist
 import           Heist.Interpreted hiding (textSplice)
+import           Data.List (intercalate)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as L
@@ -53,7 +54,7 @@ type AppH = Handler App App
 -- | Application initialization.  You need to supply path to the
 -- binary dictionary.
 appInit :: FilePath -> SnapletInit App App
-appInit binPath = makeSnaplet "myapp" "My example application" Nothing $ do
+appInit binPath = makeSnaplet "hist-pl" "HistPL" Nothing $ do
     hs <- nestSnaplet "heist" heist $ heistInit "templates"
     modifyHeistState $ bindSplices
         [ ("lex-entry", lexSplice) ]
@@ -77,27 +78,65 @@ echoHandler = do
     maybe (writeBS "must specify echo/param in URL") writeBS param
 
 
--- | Show various information concerning the lexeme specified by the
--- @lexid@ parameter.
+-- | Show information about lexemes specified by an identifier or a form.
 lexSplice :: Splice AppH
-lexSplice = report . runEitherT $ do
-    hpl   <- lift   $ gets _histPL
-    lexID <- lift (getParam "id") >>=
-        tryJust "Param @id not specified"
+lexSplice = failLeft . runEitherT $ lexByID <|> lexByForm
+
+
+-- | Show information about lexemes specified by a lexeme identifier.
+lexByID :: EitherT [String] (HeistT AppH AppH) Template
+lexByID = do
+    hpl   <- gets _histPL
+    lexID <- lift (getParam "id") >>= tryJust ["Param @id not specified"]
     entry <- liftIO (H.tryLoad' hpl $ T.decodeUtf8 lexID) >>=
-        tryJust "No etry with given @id"
-    hoistEither $ X.parseHTML "-" (decorate entry)
+        tryJust ["No etries with given @id"]
+    hoistEither $ lexToHTML entry
+
+
+-- | Show information about lexemes specified by a form.
+lexByForm :: EitherT [String] (HeistT AppH AppH) Template
+lexByForm = do
+    hpl <- gets _histPL
+    form <- lift (getParam "form") >>= tryJust ["Param @form not specified"]
+    entries <- liftIO $ H.lookup hpl $ T.decodeUtf8 form
+    hoistEither $ decorate entries
   where
+    decorate xs = do
+        let h = numInfo xs
+        nodes <- concat <$> mapM decorateEntry xs
+        return $ h : nodes
+    numInfo xs =
+        let n = T.pack $ show $ length xs
+            info = "Number of interpretations: " `T.append` n
+        in  X.Element "h3" [] [X.TextNode info]
+    decorateEntry (entry, code) = do
+        desc <- lexToHTML entry
+        let h = case code of
+                H.Copy  -> "Possible interpretation (based on PoliMorf):"
+                _       -> "Historical interpretation:"
+        return $ X.Element "h5" [] [X.TextNode h] : desc
+
+
+-- | Fail on left value.
+failLeft :: Monad m => m (Either [String] b) -> m b
+failLeft m = m >>= \x -> case x of
+    Left es -> fail (intercalate ", " es)
+    Right y -> return y
+
+
+-- | Translate entry to a Heist template (a list of HTML nodes).
+lexToHTML :: H.LexEntry -> Either [String] Template
+lexToHTML entry
+    = both (:[]) X.docContent
+    $ X.parseHTML "-" (decorate entry)
+  where
+    both f _ (Left x)   = Left  (f x)
+    both _ g (Right x)  = Right (g x)
     decorate = B.fromString
-             . Pandoc.writeHtmlString Pandoc.def
-             . Pandoc.readMarkdown Pandoc.def
-             . (\txt -> "~~~~~ {.xml}\n" ++ txt ++ "\n~~~~~\n")
-             . L.unpack . H.showLexEntry
-    report app = app >>= \x -> case x of
-        Right y -> return $ X.docContent y
-        Left e  -> fail e
-
-
+        . Pandoc.writeHtmlString Pandoc.def
+        . Pandoc.readMarkdown Pandoc.def
+        . (\txt -> "~~~~~ {.xml}\n" ++ txt ++ "\n~~~~~\n")
+        . L.unpack . H.showLexEntry
 
 
 -- | Factorial splice.
