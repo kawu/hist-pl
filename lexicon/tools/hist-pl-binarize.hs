19d4b66475360e14863f0f2eb3e34e223ff26e26
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-05-29 10:22:35 +0200

    Remove old tools

--- lexicon/tools/hist-pl-binarize.hs
+++
@@ -1,7 +0,0 @@
-import           System.Environment (getArgs)
-import           NLP.HistPL.LMF (readLMF)
-import qualified NLP.HistPL as H
-
-main = do
-    [lmfPath, binPath] <- getArgs
-    H.save binPath =<< readLMF lmfPath
