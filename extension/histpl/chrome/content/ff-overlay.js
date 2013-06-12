247380d0a1668b8b5b3a36b6601aa3557ef639c2
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-06-12 13:31:40 +0200

    Initial version of the extention

---
+++ extension/histpl/chrome/content/ff-overlay.js
@@ -0,0 +1,36 @@
+var histpl = {
+  onLoad: function() {
+    // initialization code
+    this.initialized = true;
+    this.strings = document.getElementById("histpl-strings");
+  },
+
+  onMenuItemCommand: function(e) {
+    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                  .getService(Components.interfaces.nsIPromptService);
+    promptService.alert(window, this.strings.getString("helloMessageTitle"),
+                                this.strings.getString("helloMessage"));
+  },
+
+  onToolbarButtonCommand: function(e) {
+    // just reuse the function above.  you can change this, obviously!
+    histpl.onMenuItemCommand(e);
+  }
+};
+
+window.addEventListener("load", function () { histpl.onLoad(); }, false);
+
+
+histpl.onFirefoxLoad = function(event) {
+  document.getElementById("contentAreaContextMenu")
+          .addEventListener("popupshowing", function (e) {
+    histpl.showFirefoxContextMenu(e);
+  }, false);
+};
+
+histpl.showFirefoxContextMenu = function(event) {
+  // show or hide the menuitem based on what the context menu is on
+  document.getElementById("context-histpl").hidden = gContextMenu.onImage;
+};
+
+window.addEventListener("load", function () { histpl.onFirefoxLoad(); }, false);
---
+++ extension/histpl/defaults/preferences/prefs.js
@@ -0,0 +1,11 @@
+// Default preference values. These are accessible via the preferences system
+// or via the optional chrome/content/options.xul preferences dialog.
+
+// These are here as an example only. Please remove them or rename them to
+// something useful.
+pref("extensions.histpl.boolpref", false);
+pref("extensions.histpl.intpref", 0);
+pref("extensions.histpl.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.histpl@waszczuk.kuba.pl.description", "chrome://histpl/locale/overlay.properties");
