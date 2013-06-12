90504bee9e9ba276d7d39c0db5202bc43cfb1049
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-06-12 19:22:09 +0200

    Add forward and backward buttons

--- extension/histpl/chrome/content/hwin.js
+++ extension/histpl/chrome/content/hwin.js
@@ -9,3 +9,15 @@ var hwin = {
 
 var key = window.arguments[0];
 window.addEventListener("load", function () { hwin.onLoad(key); }, false);
+
+// Move the browser backward.
+hwinGoBack = function () {
+    var browser = document.getElementById('browser');
+    browser.goBack();
+}
+
+// Move the browser forward.
+hwinGoForward = function () {
+    var browser = document.getElementById('browser');
+    browser.goForward();
+}
